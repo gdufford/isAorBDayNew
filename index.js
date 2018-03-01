c6b51972b8c4c7cb25060bad8c7a22821194c6e1
Garry Dufford <gdufford@gmail.com>
2018-03-01 13:05:06 -0700

    Working pretty good with DB and reasonable error checking

--- index.js
+++ index.js
@@ -9,25 +9,90 @@ var pool = mysql.createPool({
 });
 
 exports.handler = function(event, context, callback){
+  var passedDate, reply;
   switch(event.request.type){
     case "IntentRequest":
       console.log('--------The intent request was captured--------');
       switch(event.request.intent.name){
         case "GetAorBDay":
+          var dateHolder = new Date();
+          passedDate = dateHolder.getFullYear() + '-' + (dateHolder.getMonth() + 1) + "-" + dateHolder.getDate();
+          reply = "Today is ";
           console.log('----------Calling for today-----------');
-          context.succeed(generateResponse(buildSpeechletResponseToday(context)));
+          //context.succeed(generateResponse(buildSpeechletResponseToday(context)));
           break;
         case "GetAorBDayDate":
-          var passedDate = event.request.intent.slots.TheDate.value;
+          passedDate = event.request.intent.slots.TheDate.value;
+          reply = "That is ";
           console.log('-------Passed Date: ' + passedDate + "-------------");
-          context.succeed(generateResponse(buildSpeechletResponseDate(passedDate,context)));
+          //context.succeed(generateResponse(buildSpeechletResponseDate(passedDate,context)));
           break;
       }
     break;
   }
-};
 
-generateResponse = function(speechletResponse) {
+
+  //console.log('----IN getAorBDay FUNCTION----');
+  context.callbackWaitsForEmptyEventLoop = false;
+  pool.getConnection(function(err, connection) {
+    if (err) { console.log('----getAorBDay ERROR----'); throw err; }
+    //STR_TO_DATE('2018-02-19','%Y-%m-%d')
+    var query = "select * from all_dates where day_date = STR_TO_DATE('" + passedDate + "','%Y-%m-%d')";
+    console.log('-----query: ' + query + '-----------------');
+
+    connection.query(query,function(error, results, fields){
+      console.log('-----IN QUERY------');
+      connection.release();
+
+      if(error) {
+        console.log('-----IN ERROR------');
+        callback(error);
+      } else {
+        console.log('-----no error------');
+        console.log('-----Count: ' + results.length + '-----');
+        //console.log('-----RESULT: ' + results[0].day_type + "--------" + results[0].day_date + '--------------');
+
+        try {
+          var dayType = results[0].day_type;
+        }  catch(error){
+
+        }
+
+        if (dayType === 'A') {
+          reply += 'an A day';
+        } else if (dayType === 'B') {
+          reply += 'a B day';
+        } else if (dayType === 'O' || dayType === 'S') {
+          reply += 'a day without school';
+        } else {
+          reply += 'a day I dont know about. Might be a weekend or day off from school';
+        }
+
+        //return reply;// + "----" + results[0].day_date);
+        context.succeed(
+          {
+            version: "1.0",
+            sessionAttributes: {},
+            response: {
+              shouldEndSession: true,
+              outputSpeech: {
+                type: "PlainText",
+                text: reply
+              }
+            }
+          }
+        );
+      }
+      //console.log(results[0].day_type + "--" + results[0].day_date); //Old local line, not lambda specific
+
+      //process.exit(); //used for local, not for Lambda
+    });
+
+
+
+});
+
+/*generateResponse = function(speechletResponse) {
   return {
       version: "1.0",
       sessionAttributes: {},
@@ -60,36 +125,6 @@ buildSpeechletResponseDate = function(passedDate,context) {
 };
 
 getAorBDay = function(passedDate,context){
-  console.log('----IN getAorBDay FUNCTION----');
-  context.callbackWaitsForEmptyEventLoop = false;
-  pool.getConnection(function(err, connection) {
-    if (err) { console.log('----getAorBDay ERROR----'); throw err; }
-
-    var query = 'select * from all_dates where day_date = ' + passedDate;
-    console.log('-----query: ' + query + '-----------------');
-
-    connection.query(query,function(error, results, fields){
-      connection.release();
-
-      if(error) {
-        callback(error);
-      } else {
-        console.log('-----no error------');
-        console.log('-----RESULT: ' + results[0].day_type + "--------" + results[0].day_date + '--------------');
-
-        var reply = "That is ";
-        var dayType = results[0].day_type;
-
-        if (dayType === 'A') reply += "an A day";
-        if (dayType === 'B') reply += "a B day";
-        if (dayType === 'O' || dayType === 'S') reply += "a day without school";
-
-        return reply;// + "----" + results[0].day_date);
-      }
-      //console.log(results[0].day_type + "--" + results[0].day_date); //Old local line, not lambda specific
-
-      //process.exit(); //used for local, not for Lambda
-    });
   });
 
   //--OLD CODE-PRE-DB------//
